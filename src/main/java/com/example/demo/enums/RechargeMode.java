41952e1e959057690c82d4f0d4726e143a518f25
Canon4G <gllawliet@163.com>
2018-12-14 17:12:01 +0800

    新增充值方式枚举类

---
+++ src/main/java/com/example/demo/enums/RechargeMode.java
@@ -0,0 +1,39 @@
+package com.example.demo.enums;
+
+/**
+ * 充值方式枚举
+ * create by Canon4G 2018-12-14
+ **/
+public enum RechargeMode {
+
+    WEI_CHAT("0", "微信"),
+    ALI_PAY("1", "支付宝"),
+    BANK("3", "银行卡");
+
+
+    private String value;
+    private String desc;
+
+    RechargeMode(String value, String desc) {
+        this.value = value;
+        this.desc = desc;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public RechargeMode setValue(String value) {
+        this.value = value;
+        return this;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public RechargeMode setDesc(String desc) {
+        this.desc = desc;
+        return this;
+    }
+}
