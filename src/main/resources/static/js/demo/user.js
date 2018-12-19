b441e888f2e1e3b908d4892d0bd2574d58c4190a
Canon4G <gllawliet@163.com>
2018-12-19 22:43:31 +0800

    新增页面 用户管理

---
+++ src/main/resources/static/js/demo/user.js
@@ -0,0 +1,159 @@
+$(function () {
+    init();
+});
+
+function init() {
+    getUserList(1);
+}
+
+// TODO: 分页
+function getUserList(pageNum) {
+    $("#userManagerList tbody").empty();
+}
+
+function userPageClick(page) {
+    getUserList(page);
+}
+
+function checkUserTypeText(s) {
+    if ('0' === s) {
+        return "超级管理员";
+    }
+    if ('1' === s) {
+        return "管理员";
+    }
+    if ('2' === s) {
+        return "普通用户";
+    }
+}
+
+function showAddUserWin() {
+    $("#addUserName").val("");
+    $("#addUserPwd").val("");
+    $("#addUserPwd2").val("");
+    $("#mask").show();
+    $("#addUserWin").show();
+}
+
+function toAddUser() {
+    $.ajax({
+        type: 'POST',
+        url: '/register',
+        data: {
+            "username": $("#addUserName").val(),
+            "password": $("#addUserPwd").val(),
+            "rePassword": $("#addUserPwd2").val(),
+            "isAdmin": $("#addUserType").val()
+        },
+        success: function (data) {
+            if ('success' !== data.code) {
+                alert(data.data.returnMsg);
+                return;
+            }
+            alert(data.data.returnMsg);
+            closeAddUserWin();
+            init();
+        }
+    });
+}
+
+function closeAddUserWin() {
+    $("#addUserWin").hide();
+    $("#mask").hide();
+}
+
+function showUpdateUserWin(userCode) {
+    $.ajax({
+        type: 'POST',
+        url: '/getUserDetails',
+        data: {
+            "userCode": userCode
+        },
+        success: function (data) {
+            if ('success' !== data.code) {
+                return;
+            }
+            $("#updateUserName").val(data.data.user.userName);
+            $("#updateUserType").val(checkUserTypeText(data.data.user.isAdmin));
+            $("#updateUserPwd").val("");
+            $("#updateUserPwd2").val("");
+            $("#updateUserCode").val(data.data.user.userCode);
+            $("#mask").show();
+            $("#updateUserWin").show();
+        }
+    });
+}
+
+function toUpdateUser() {
+    $.ajax({
+        type: 'POST',
+        url: '/updateUser',
+        data: {
+            "userCode": $("#updateUserCode").val(),
+            "userName": $("#updateUseName").val(),
+            "password": $("#updateUserPwd").val(),
+            "password2": $("#updateUserPwd2").val(),
+            "password3": $("#updateUserPwd3").val(),
+            "isAdmin": $("#updateUserType").val(),
+            "isAdmin2": $("#updateUserType2").val()
+        },
+        success: function (data) {
+            if ('success' !== data.code) {
+                alert(data.data.returnMsg);
+                return;
+            }
+            alert(data.data.returnMsg);
+            closeUpdateUserWin();
+            init();
+        }
+    });
+}
+
+function closeUpdateUserWin() {
+    $("#addUserWin").hide();
+    $("#mask").hide();
+}
+
+function toDeleteUser(userCode) {
+    var r = confirm("是否删除用户信息");
+    if (!r) {
+        init();
+    } else {
+        $.ajax({
+            type: 'POST',
+            url: '/deleteUser',
+            data: {
+                "userCode": userCode
+            },
+            success: function (data) {
+                if ('success' !== data.code) {
+                    alert(data.data.returnMsg);
+                    return;
+                }
+                alert(data.data.returnMsg);
+                init();
+            }
+        });
+    }
+}
+
+/**
+ * 查询下拉列表
+ */
+$("#userSelectManager").on("change", function () {
+    $("#userTypeManager").val($("option:selected",this).val());
+});
+
+/**
+ * 添加下拉列表
+ */
+$("#addUserSelect").on("change", function () {
+    $("#addUserType").val($("option:selected",this).val());
+});
+
+/**
+ * 修改下拉列表
+ */
+$("#updateUserSelect").on("change", function () {
+    $("#updateUserType2").val($("option:selected",this).val());
+});
